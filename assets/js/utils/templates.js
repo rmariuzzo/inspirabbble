54e64baacaf06ee7f591dab0267352c62d433719
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-03-01 23:04:55 -0400

    Adding dripicons as bower dependency, new task created to copy static resource from vendors.

--- assets/js/utils/templates.js
+++ assets/js/utils/templates.js
@@ -3,14 +3,17 @@
 define(
     [
         'hbs!../../../views/partials/shot',
-        'hbs!../../../views/partials/grid'
+        'hbs!../../../views/partials/grid',
+        'hbs!../../../views/partials/header'
     ], function(
         shot,
-        grid
+        grid,
+        header
     ) {
         return {
             shot: shot,
-            grid: grid
+            grid: grid,
+            header: header
         };
     }
 );
