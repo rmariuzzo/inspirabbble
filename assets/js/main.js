dae9b8e207984273d3ed4e97068b66c83fba7814
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-06-22 22:15:33 -0400

    Documentation added into main entry point.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
+/**
+ * The main entry point when everything comes to life ;)
+ */
+
 'use strict';
 
 // Load requirejs configuration.
 require(['config'], function() {
 
+    // Load Inspirabbble app and jQuery.
     require(['inspirabbble', 'jquery'], function(Inspirabbble, $) {
 
         // Start Inspirabbble when DOM is ready.
