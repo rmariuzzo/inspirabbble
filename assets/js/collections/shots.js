edbea00c77d4c1e60a52a17f70ce78cd3507aa8f
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-06-29 01:17:51 -0400

    Improved shots collection and shot model to act as true Backbone objects.

--- assets/js/collections/shots.js
+++ assets/js/collections/shots.js
@@ -10,28 +10,145 @@ define([
     'models/shot'
 ], function(_, Backbone, Shot) {
 
+    /**
+     * The shots collection.
+     */
     var Shots = Backbone.Collection.extend({
 
+        /**
+         * The model class this collection contains.
+         */
         model: Shot,
-        id: null,
-        page: 1,
-        perPage: 30,
-        host: '//api.dribbble.com',
-        endpoint: '/shots/:id',
 
+        /**
+         * The parameter identifier to use when fetching.
+         */
+        id: null,
+
+        /**
+         * The page to fetch when using pagination.
+         */
+        page: 1,
+
+        /**
+         * The number of shots to fetch when using pagination.
+         */
+        perPage: 30,
+
+        /**
+         * The host name of the Dribbble API to use when fetching.
+         */
+        host: '//api.dribbble.com',
+
+        /**
+         * The endpoint to use when fetching.
+         */
+        endpoint: null,
+
+        /**
+         * Return the URL to fetch shots.
+         */
         url: function() {
             return this.host + this.endpoint.replace(/:\w+/, this.id);
         },
 
+        /**
+         * Parse the raw fetch response.
+         */
         parse: function(response) {
-            return response.shots;
+            return response.shots || response;
         },
 
+        /**
+         * Fetch shots from Dribbble API.
+         *
+         * @params options object The fetch options.
+         */
         fetch: function(options) {
-            options = _.extend({
+            // Ensure we have an endpoint and an id.
+            if (!this.endpoint || !this.id) {
+                throw new Error('Endpoint and/or id are missing.');
+            }
+
+            // Force the AJAX call to be of JSONP type.
+            options = _.extend(options || {}, {
                 dataType: 'jsonp'
-            }, options);
+            });
+
+            // Call the original fetch method.
             Backbone.Collection.prototype.fetch.call(this, options);
+        },
+
+        /**
+         * Fetch a single shot.
+         *
+         * @param id number The id of the shot.
+         * @param options object The fetch options.
+         */
+        fetchShot: function(id, options) {
+            this.id = id;
+            this.endpoint = '/shots/:id';
+            this.fetch(options);
+        },
+
+        /**
+         * Fetch shot rebounds.
+         *
+         * @param id number The id of the shot.
+         * @param options object The fetch options.
+         */
+        fetchRebounds: function(id, options) {
+            this.id = id;
+            this.endpoint = '/shots/:id/rebounds';
+            this.fetch(options);
+        },
+
+        /**
+         * Fetch shots in a list.
+         *
+         * @param list strign The name of the list.
+         * @param options object The fetch options.
+         */
+        fetchShotsByList: function(list, options) {
+            this.id = list;
+            this.endpoint = '/shots/:list';
+            this.fetch(options);
+        },
+
+        /**
+         * Fetch shots from a player.
+         *
+         * @param id number The id of the player.
+         * @param options object The fetch options.
+         */
+        fetchShots: function(id, options) {
+            this.id = id;
+            this.endpoint = '/players/:id/shots';
+            this.fetch(options);
+        },
+
+        /**
+         * Fetch shots followed by a player
+         *
+         * @param id number The id of the player.
+         * @param options object The fetch options.
+         */
+        fetchShotsFollowing: function(id, options) {
+            this.id = id;
+            this.endpoint = '/players/:id/shots/following';
+            this.fetch(options);
+        },
+
+        /**
+         * Fetch shots liked by a player.
+         *
+         * @param id number The id of the player.
+         * @param options object The fetch options.
+         */
+        fetchShotsLiked: function(id, options) {
+            this.id = id;
+            this.endpoint = '/players/:id/shots/likes';
+            this.fetch(options);
         }
 
     });
--- assets/js/models/shot.js
+++ assets/js/models/shot.js
@@ -21,6 +21,22 @@ define([
             imageUrl: null,
             imageTeaserUrl: null,
             image400Url: null
+        },
+
+        parse: function(data) {
+            /* jshint -W106 */
+            return {
+                id: data.id,
+                title: data.title,
+                description: data.description,
+                width: data.width,
+                height: data.height,
+                url: data.url,
+                imageUrl: data.image_400_url,
+                imageTeaserUrl: data.image_teaser_url,
+                image400Url: data.image_400_url
+            };
+            /* jshint +W106 */
         }
 
     });
