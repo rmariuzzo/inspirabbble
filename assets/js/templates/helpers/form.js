37613d832a982933049f28e8b3fabc948b3e3a67
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-07-17 21:06:21 -0400

    Settings improved & simplified.

--- assets/js/templates/helpers/form.js
+++ assets/js/templates/helpers/form.js
@@ -5,14 +5,26 @@
 'use strict';
 
 define([
+    'underscore',
     'hbs/handlebars',
     'hbs!templates/input'
-], function(Handlebars, inputTemplate) {
+], function(_, Handlebars, inputTemplate) {
 
     Handlebars.registerHelper('form', function() {
         if (this.type === 'checkbox') {
-            this.checkbox = true;
+            this.isCheckbox = true;
             this.checked = !!this.value;
+        }
+        if (this.type === 'select') {
+            this.isSelect = true;
+            _.each(this.options, function(option) {
+                if (option.value === this.value) {
+                    option.selected = true;
+                }
+            }.bind(this));
+        }
+        if (this.type === 'text') {
+            this.isText = true;
         }
         return new Handlebars.SafeString(inputTemplate(this));
     });
